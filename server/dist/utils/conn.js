e67777b2ab5d21e02fe31f3ad3ff93e8f3f03bb7
HassanHabibTahir <hassanhabibtahir@gmail.com>
2024-10-19 19:17:48 +0500

    testDsol deply ts server

--- server/dist/utils/conn.js
+++ server/dist/utils/conn.js
@@ -5,8 +5,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectDB = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
-// const url = process.env.MONGO_URI as string;
-const url = "mongodb+srv://nftthee:nftthee@cluster0.hov9i7k.mongodb.net/";
+const url = process.env.MONGO_URI;
+// const url ="mongodb+srv://nftthee:nftthee@cluster0.hov9i7k.mongodb.net/"
 function connectDB() {
     return new Promise((resolve, reject) => {
         mongoose_1.default.set("strictQuery", false);
